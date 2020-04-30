1624720314bff31c4fc62f89a1890319e2360bd4
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-04-30 09:10:21 +0200

    Rewriting transformation function properly

--- pyper/representations/persistence_image.py
+++ pyper/representations/persistence_image.py
@@ -1,44 +1,62 @@
 """Persistence image class."""
 
-
 import numpy as np
 
 
-class PersistenceImageTransformer:
-    '''
-    Functor for creating a persistence image from a persistence diagram.
-    '''
+def _gaussian_kernel(x, y, ux, uy, sigma):
+    """Gaussian kernel for a 2D point."""
+    return 1 / (2 * np.pi * sigma**2) * np.exp(
+        -((x - ux)**2 + (y - uy)**2) / (2 * sigma**2)
+    )
 
-    def __init__(self, resolution, **kwargs):
-        self._width = resolution    # x resolution
-        self._height = resolution   # y resolution
+
+def _transform(persistence_diagram):
+    """Transform persistence diagram for persistence image calculation."""
+    persistence_diagram[:, 1] -= persistence_diagram[:, 0]
+    return persistence_diagram
+
+
+class PersistenceImage:
+    """Functor for creating a persistence image from a persistence diagram."""
+
+    def __init__(self, resolution=(10, 10), **kwargs):
+        """Create new persistence image with parameters."""
+        # Set the width first as is custom for images. This is deviating
+        # from the way we would handle images here.
+        self.width, self.height = resolution
 
         # Access minimum and maximum coordinates for the functor, if
         # specified. They are used to ensure that *all* diagrams are
         # on the same scale.
-        self._xmin = kwargs.get('xmin')
-        self._xmax = kwargs.get('xmax')
-        self._ymin = kwargs.get('ymin')
-        self._ymax = kwargs.get('ymax')
+        self.xmin = kwargs.get('xmin')
+        self.xmax = kwargs.get('xmax')
+        self.ymin = kwargs.get('ymin')
+        self.ymax = kwargs.get('ymax')
 
     @staticmethod
-    def kernel(x, y, ux, uy, sigma):
-        '''
-        Kernel function for evaluating a single point. This cannot be
-        changed for now.
-        '''
+    def w(y, b):
+        """Weight function for an individual point in the persistence image.
 
-        return 1 / (2 * math.pi * sigma**2) * np.exp(
-            -((x - ux)**2 + (y - uy)**2) / (2 * sigma**2)
-        )
+        This is the default weight function described in the original
+        paper [1]_.
 
-    @staticmethod
-    def w(x, y, b):
-        '''
-        Weight function for weighting an individual point in
-        a persistence diagram.
-        '''
+        Parameters
+        ----------
+        y : float
+            Persistence coordinate of the respective point.
 
+        b : float
+            Maximum persistence coordinate to use for the cut-off value
+            of the weight function.
+
+        Returns
+        -------
+        Weight for the specified point in the persistence image.
+
+        [1]: H. Adams et al., 'Persistence Images: A Stable Vector
+        Representation of Persistent Homology', Journal of Machine
+        Learning Research 18 (2017), pp. 1-35.
+        """
         if y <= 0:
             return 0.0
         elif 0 < y and y < b:
@@ -46,25 +64,46 @@ class PersistenceImageTransformer:
         else:
             return 1.0
 
-    def transform(self, persistence_diagram, max_persistence):
+    def transform(self, persistence_diagrams, max_persistence=None):
+        """Calculate persistence images of a sequence of diagrams."""
 
-        points = np.array(persistence_diagram._pairs)
+    def _transform(self, persistence_diagram, max_persistence):
+        """Calculate persistence image of a single diagram.
 
-        xmin, ymin = self._xmin, self._ymin
-        xmax, ymax = self._xmax, self._ymax
+        Parameters
+        ----------
+        persistence_diagram : `PersistenceDiagram` or `np.array`
+            Persistence diagram whose persistence image should be
+            calculated. This can also be a data type that one can
+            convert to a 2D `numpy` array.
 
-        if not xmin:
-            xmin = np.min(points[:, 0])
+        max_persistence : float
+            Specifies the maximum persistence value to consider for the
+            conversion process. This is required for weighting points.
 
-        if not xmax:
-            xmax = np.max(points[:, 0])
+        Returns
+        -------
+        Persistence image.
+        """
+        persistence_diagram = _transform(np.asarray(persistence_diagram))
 
-        if not ymin:
-            ymin = np.min(points[:, 1])
+        xmin, ymin = self.xmin, self.ymin
+        xmax, ymax = self.xmax, self.ymax
 
-        if not ymax:
-            ymax = np.max(points[:, 1])
+        if xmin is None:
+            xmin = np.min(persistence_diagram[:, 0])
 
+        if xmax is None:
+            xmax = np.max(persistence_diagram[:, 0])
+
+        if ymin is None:
+            ymin = np.min(persistence_diagram[:, 1])
+
+        if ymax is None:
+            ymax = np.max(persistence_diagram[:, 1])
+
+        # Create the proper grid for the persistence image. This will be
+        # 'flipped' later on when it comes to the matrix conversion.
         x, xstep = np.linspace(xmin, xmax, self._width, retstep=True)
         y, ystep = np.linspace(ymin, ymax, self._height, retstep=True)
 
@@ -81,7 +120,8 @@ class PersistenceImageTransformer:
         xc += xmin
         yc += ymin
 
-        pit = np.zeros((self._width, self._height))
+        # TODO: should we flip this here?
+        persistence_image = np.zeros((self.height, self.width))
 
         for i in range(self._width):
             for j in range(self._height):
@@ -92,12 +132,12 @@ class PersistenceImageTransformer:
 
                 # Evaluate the kernel function for *every* point of the
                 # discretized domain.
-                for c, d in points:
-                    f = PersistenceImageTransform.w(c, d, max_persistence) \
-                      * PersistenceImageTransform.kernel(cx, cy, c, d, 0.2)
+                for c, d in persistence_diagram:
+                    f = PersistenceImage.w(c, d, max_persistence) \
+                      * _gaussian_kernel(cx, cy, c, d, 0.2)
 
                     value += f
 
-                pit[i, j] = value
+                persistence_image[i, j] = value
 
-        return pit
+        return persistence_image
