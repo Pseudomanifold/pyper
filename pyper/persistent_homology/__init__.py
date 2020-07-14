e7eab38bc7c9e73b760e12f6a27d5dcd427e4c19
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-07-14 11:54:39 +0200

    Exporting persistence diagram calculation function

--- pyper/persistent_homology/__init__.py
+++ pyper/persistent_homology/__init__.py
@@ -4,8 +4,11 @@ from .graphs import calculate_distance_filtration
 from .graphs import calculate_height_filtration
 from .graphs import calculate_persistence_diagrams
 
+from .functions import calculate_persistence_diagrams_1d
+
 __all__ = [
     'calculate_distance_filtration',
     'calculate_height_filtration',
-    'calculate_persistence_diagrams'
+    'calculate_persistence_diagrams',
+    'calculate_persistence_diagrams_1d'
 ]
