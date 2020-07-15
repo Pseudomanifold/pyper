68032beea25f3e697c333e7f0eb9f834aa2786d0
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-07-15 11:23:48 +0200

    Documented test case

--- tests/persistent_homology/test_functions.py
+++ tests/persistent_homology/test_functions.py
@@ -6,6 +6,17 @@ from pyper.persistent_homology import calculate_persistence_diagrams_1d
 
 
 class TestBeketayev(unittest.TestCase):
+    """Test case based on paper by Beketayev et al.
+
+    The test comprises two functions whose persistence diagrams are
+    equal. See
+
+        Measuring the Distance between Merge Trees
+        Kenes Beketayev et al.
+
+    for more details.
+    """
+
     def test(self):
         for order in ['sublevel', 'superlevel']:
 
